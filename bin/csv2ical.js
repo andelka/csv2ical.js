d1618d568298e33ef9d77f5129b75c34e9f1ecb8
andelka <andelka@users.noreply.github.com>
2019-10-22 14:19:13 +0200

    update method names

--- bin/csv2ical.js
+++ bin/csv2ical.js
@@ -13,7 +13,20 @@ const moment = require('moment');
 const csv = require('fast-csv');
 const ical = require('ical-generator');
 const fs = require('fs');
-const argv = require('yargs').usage('Usage: csv2ical.js -i [csv] -o [ics] -d [delimiter] -H').default('d', ',').default('H', false).default('rows', ['0', '1', '2', '3', '4']).demand(['i', 'o']).describe('i', 'Input CSV file').describe('o', 'Output ICS file').describe('d', 'If your data uses an alternate delimiter such as ";"').describe('H', 'To set if the first line of your CSV contain headers').describe('rows', 'Row numbers for "Subject","Start Date","End Date" ... : --rows={0,1,2,3,4}').describe('dateformat', 'Date format 31/07/2014 03:00 : --dateformat="DD-MM-YYYY HH:mm"').example('csv2ical.js -i sample.csv -o sample.ics', '-d ";" -H ').argv;
+const argv = require('yargs')
+  .usage('Usage: csv2ical.js -i [csv] -o [ics] -d [delimiter] -H')
+  .default('d', ',')
+  .default('H', true)
+  .default('rows', ['0', '1', '2', '3', '4'])
+  .demand(['i', 'o'])
+  .describe('i', 'Input CSV file')
+  .describe('o', 'Output ICS file')
+  .describe('d', 'If your data uses an alternate delimiter such as ";"')
+  .describe('H', 'To set if the first line of your CSV contain headers')
+  .describe('rows', 'Row numbers for "Subject","Start Date","End Date" ... : --rows={0,1,2,3,4}')
+  .describe('dateformat', 'Date format 31/07/2014 03:00 : --dateformat="DD-MM-YYYY HH:mm"')
+  .example('csv2ical.js -i sample.csv -o sample.ics', '-d ";" -H ')
+  .argv;
 
 /**
  * Init Stuff
@@ -22,23 +35,23 @@ const stream = fs.createReadStream(argv.i);
 // const log = new winston.Logger();
 const cal = ical();
 
-cal.setProdID({
+cal.prodId({
   company: 'My Company',
   product: 'My Product',
   language: 'EN'
 });
-cal.setDomain('mycompany.com').setName('My Calendar');
+cal.domain('mycompany.com').name('My Calendar');
 
 /**
 * Parse CSV file and convert to ical events
 */
-csv.fromStream(stream, { headers: argv.H, delimiter: argv.d }).on('data', data => {
+csv.parseStream(stream, { headers: argv.H, delimiter: argv.d }).on('data', data => {
   /**
   * Only process data if StartDate starts with a number
   */
   if (data[Object.keys(data)[argv.rows[1]]].match(/^[1-9]/)) {
     // "Subject","Start Date","End Date","Description"
-    // console.log(data);
+    console.log(data);
     log.info(`Subject : ${ data[Object.keys(data)[argv.rows[0]]] }`); // Subject
     log.info(`Start Date : ${ data[Object.keys(data)[argv.rows[1]]] }`); // Start Date
     log.info(`End Date : ${ data[Object.keys(data)[argv.rows[2]]] }`); // End Date
@@ -50,7 +63,7 @@ csv.fromStream(stream, { headers: argv.H, delimiter: argv.d }).on('data', data =
     const enddate = moment(data[Object.keys(data)[argv.rows[2]]], argv.dateformat);
 
     if (startdate.isValid() && enddate.isValid()) {
-      cal.addEvent({
+      cal.createEvent({
         start: new Date(startdate.toISOString()),
         end: new Date(enddate.toISOString()),
         summary: data[Object.keys(data)[argv.rows[0]]],
